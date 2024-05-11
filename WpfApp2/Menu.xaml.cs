ab97062efe5b917b9e0b7630efbb9f3a30172485
VikaDrozhak <vt222_dvt@student.ztu.edu.ua>
2024-05-11 13:40:34 +0300

    adding classes for database and account

---
+++ WpfApp2/Menu.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LibraryBank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        Bank bank = new Bank("MyBank");
+        AutomatedTellerMachine atm;
+        List<Account> accounts;
+        string cardNumber;
+        DatabaseHelper databaseHelper;
+        private Account clientAccount;
+        public Menu(string cardNumber, List<Account> accounts, AutomatedTellerMachine atm, DatabaseHelper databaseHelper)
+        {
+            InitializeComponent();
+
+            this.cardNumber = cardNumber;
+            this.accounts = accounts;
+            this.atm = atm;
+            this.databaseHelper = databaseHelper;
+            this.clientAccount = clientAccount;
+        }
+    }
+}
